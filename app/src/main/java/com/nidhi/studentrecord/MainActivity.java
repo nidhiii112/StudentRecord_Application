694f2fd1fb8393cd51e3329f7c7e0a76e04f5c4a
Nidhi singh <117963273+nidhiii112@users.noreply.github.com>
2024-01-08 22:22:31 +0530

    Update MainActivity.java

--- app/src/main/java/com/nidhi/studentrecord/MainActivity.java
+++ app/src/main/java/com/nidhi/studentrecord/MainActivity.java
@@ -7,7 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+import android.widget.Toast; 
 
 public class MainActivity extends AppCompatActivity {
     DatabaseHelper myDB;
