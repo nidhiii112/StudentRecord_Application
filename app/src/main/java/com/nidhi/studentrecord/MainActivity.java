c1e3b0d2971336b6018e71633839f67147038744
Nidhi singh <117963273+nidhiii112@users.noreply.github.com>
2024-01-08 22:22:42 +0530

    Update MainActivity.java

--- app/src/main/java/com/nidhi/studentrecord/MainActivity.java
+++ app/src/main/java/com/nidhi/studentrecord/MainActivity.java
@@ -5,7 +5,7 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.widget.Button; 
 import android.widget.EditText;
 import android.widget.Toast; 
 
